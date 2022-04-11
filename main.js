c378b25b6cae28add4b660996a6cf0f011fd2f4d
KonaDaniel <79358753+KonaDaniel@users.noreply.github.com>
2022-04-11 17:02:35 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -6,14 +6,17 @@ leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
 
-function preload()
-{
+scoreLeftWristY = 0;
+scoreRightWristY = 0;
+songStatus1 = "";
+songStatus2 = "";
+
+function preload() {
     song1 = loadSound("believer.mp3");
     song2 = loadSound("happier.mp3");
 }
 
-function setup()
-{
+function setup() {
     canvas = createCanvas(640, 525);
     canvas.center();
 
@@ -24,21 +27,46 @@ function setup()
     poseNet.on('pose', gotPoses);
 }
 
-function draw()
-{
+function draw() {
+    songStatus1 = song1.isPlaying();
+    songStatus2 = song2.isPlaying();
+
     image(video, 0, 0, 640, 525);
+    fill("red");
+    stroke("black");
+
+    if (scoreLeftWristY > 0.01) {
+        circle(leftWristX, leftWristY, 30);
+        song2.stop();
+        if (songStatus1 == false) {
+            song1.play();
+            document.getElementById("song_name").innerHTML = "Song Name: Believer";
+        }
+    }
+
+     fill("blue");
+     stroke("black");
+
+    if (scoreRightWristY > 0.01) {
+        circle(rightWristX, rightWristY, 30);
+        song1.stop();
+        if (songStatus2 == false) {
+            song2.play();
+            document.getElementById("song_name").innerHTML = "Song Name: Happier";
+        }
+    }
 }
 
-function modelLoaded()
-{
+function modelLoaded() {
     console.log('Model is Ready!');
 }
 
-function gotPoses(results)
-{
-    if(results.length > 0)
-    {
+function gotPoses(results) {
+    if (results.length > 0) {
         console.log(results);
+
+        scoreLeftWristY = results[0].pose.keypoints[9].score;
+        scoreRightWristY = results[0].pose.keypoints[10].score;
 
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
